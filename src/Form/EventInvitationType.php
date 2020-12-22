8ff2e9936cb222730cbb32e01a02c3bc6b4aaa30
pdragun <peter.dragun@gmail.com>
2020-12-22 10:13:00 +0100

    Fix whitespace

--- src/Form/EventInvitationType.php
+++ src/Form/EventInvitationType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\EventInvitation;
 use App\Entity\SportType;
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
